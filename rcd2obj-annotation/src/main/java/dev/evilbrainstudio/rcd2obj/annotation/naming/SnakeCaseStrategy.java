a05e202ec21eec240fdc3ea703ba7ad4703055ee
andrey-yurzanov <yurzanov.andrey@gmail.com>
2023-04-02 23:01:47 +1000

    Common annotations Issue: #1

---
+++ rcd2obj-annotation/src/main/java/dev/evilbrainstudio/rcd2obj/annotation/naming/SnakeCaseStrategy.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 Andrey Yurzanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.evilbrainstudio.rcd2obj.annotation.naming;
+
+/**
+ * Implementation of @{@link NamingStrategy} for data extraction. The name represents in the form of
+ * snake_case.
+ *
+ * @author Andrey_Yurzanov
+ * @since 1.0
+ */
+public class SnakeCaseStrategy implements NamingStrategy {
+  private static final char SEPARATOR = '_';
+
+  @Override
+  public String resolve(String original) {
+    boolean match = true;
+
+    StringBuilder buffer = new StringBuilder();
+    char[] symbols = original.toCharArray();
+    for (int i = 0; i < symbols.length; i++) {
+      boolean isUpper = Character.isUpperCase(symbols[i]);
+      if (match && SEPARATOR != symbols[i]) {
+        match = isUpper;
+      }
+
+      if (isUpper && i > 0) {
+        buffer.append(SEPARATOR);
+      }
+      buffer.append(Character.toLowerCase(symbols[i]));
+    }
+
+    if (match) {
+      return original;
+    }
+    return buffer.toString();
+  }
+}
