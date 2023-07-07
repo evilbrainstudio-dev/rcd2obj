8a5b2b7211220fda3063cafb5592de509c8bcd31
VTB4059892 <aurzanov@dev.vtb.ru>
2023-07-07 12:39:14 +0300

    Render refactoring Issue: #3

--- rcd2obj-codegen/src/main/java/dev/evilbrainstudio/rcd2obj/codegen/render/JavaElementRender.java
+++ rcd2obj-codegen/src/main/java/dev/evilbrainstudio/rcd2obj/codegen/render/JavaElementRender.java
@@ -16,7 +16,13 @@
 
 package dev.evilbrainstudio.rcd2obj.codegen.render;
 
+import dev.evilbrainstudio.rcd2obj.codegen.JavaElement;
 import dev.evilbrainstudio.rcd2obj.codegen.JavaElementType;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Base abstraction for the Java elements rendering.
@@ -26,11 +32,90 @@ import dev.evilbrainstudio.rcd2obj.codegen.JavaElementType;
  */
 public interface JavaElementRender {
   /**
-   * Appends to the render element of the Java language.
+   * Appends the Java element to the render.
    *
    * @param type     element's type {@link JavaElementType}
    * @param elements some literals for the element rendering
    * @return current instance
    */
   JavaElementRender append(JavaElementType type, String... elements);
+
+  /**
+   * Appends the Java element to the render.
+   *
+   * @param type    element's type {@link JavaElementType}
+   * @param element the Java class for the element rendering
+   * @return current instance
+   */
+  default JavaElementRender append(JavaElementType type, Class<?> element) {
+    return append(type, element.getCanonicalName());
+  }
+
+  /**
+   * Appends the Java element to the render.
+   *
+   * @param element some element for rendering
+   * @return current instance
+   */
+  default JavaElementRender append(JavaElement element) {
+    if (element != null) {
+      element.render(this);
+    }
+    return this;
+  }
+
+  /**
+   * Appends the Java element to the render.
+   *
+   * @param elements  the collection of some elements for rendering
+   * @param separator the separator for rendering, appends after each element, except of the last
+   *                  element
+   * @return current instance
+   */
+  default JavaElementRender append(Collection<? extends JavaElement> elements,
+      JavaElement separator) {
+    if (elements != null && !elements.isEmpty()) {
+      Iterator<? extends JavaElement> iterator = elements.iterator();
+      while (iterator.hasNext()) {
+        iterator
+            .next()
+            .render(this);
+
+        if (iterator.hasNext() && separator != null) {
+          separator.render(this);
+        }
+      }
+    }
+    return this;
+  }
+
+  /**
+   * Appends the typed element to the render.
+   *
+   * @param type      element's type {@link JavaElementType}
+   * @param value     the value for rendering
+   * @param valueType the type of the value
+   * @return current instance
+   */
+  default JavaElementRender append(JavaElementType type, Object value, Class<?> valueType) {
+    Map<Class<?>, JavaElementRender> renders = getTypeRenders();
+    for (Entry<Class<?>, JavaElementRender> entry : renders.entrySet()) {
+      Class<?> renderType = entry.getKey();
+      if (renderType.isAssignableFrom(valueType)) {
+        JavaElementRender render = entry.getValue();
+        return render.append(type, String.valueOf(value));
+      }
+    }
+
+    return append(type, String.valueOf(value));
+  }
+
+  /**
+   * The method returns renders for specific types rendering.
+   *
+   * @return renders for specific types
+   */
+  default Map<Class<?>, JavaElementRender> getTypeRenders() {
+    return Collections.emptyMap();
+  }
 }
